64f1ba484e33d7e9edffbdb19781894e6c0eb68b
Michael Garcia <garciamichael.developer@gmail.com>
2021-12-31 14:00:00 -0700

    Still figuring out stdin testing

--- pytest/test_pytest.py
+++ pytest/test_pytest.py
@@ -1,4 +1,10 @@
-import computer
+from sys import stderr, stdin
+from computer import add2,guess
+import sys
 
 def testAdd():
-    assert computer.add2(2) == 3
+    assert add2(2) == 3
+    assert add2(4) == 5
+    
+#def testGuess(capsys):
+    
