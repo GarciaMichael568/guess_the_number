b8aed534c081adf6b02b3aa39d23d58aefb0ec57
Michael Garcia <garciamichael.developer@gmail.com>
2021-12-31 17:39:24 -0700

    Finished game both user and computer guess

--- computer_and_user.py
+++ computer_and_user.py
@@ -16,6 +16,26 @@ def guess(x):
 def computer_guess(x):
     low = 1
     high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback == 'l':
+            low = guess + 1
     
+    print(f'Processing...your number is {guess}')
+        
 if __name__ == '__main__':
-    guess(2)
+    print('Welcome Gamer!')
+    game_mode = int(input('Press 1: User guess, Press 2: Computer guess: '))
+    if game_mode == 1:
+        x = int(input('Enter Max number (above 1): '))
+        guess(x)
+    elif game_mode == 2:
+        x = int(input('Enter Max Number (above 1): '))
+        computer_guess(x)
